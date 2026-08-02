eb667cc150debc013c1f19ff85732a0c6f9ebc7c
Adithya <adithyabijuch@gmail.com>
2026-08-02 21:46:30 +0530

    Edited and made user to be signed in

--- Backend/auth.py
+++ Backend/auth.py
@@ -1,30 +1,36 @@
-from jose import jwt
-from datetime import datetime,timedelta
-import os
-from dotenv import load_dotenv
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
 
-load_dotenv()
-EXPIRE = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-SECRET_KEY=os.getenv("SECRET_KEY")
-ALGORITHM=os.getenv("ALGORITHM")
-EXPIRE=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+from Backend.database import get_db
+from Backend.models import User
 
-def create_access_token(data:dict):
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-    to_encode=data.copy()
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
 
-    expire=datetime.utcnow()+timedelta(minutes=EXPIRE)
-
-    to_encode.update({"exp":expire})
-
-    token=jwt.encode(
-        to_encode,
-        SECRET_KEY,
-        algorithm=ALGORITHM
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
-    
 
-    return token
-print(os.getenv("SECRET_KEY"))
-print(os.getenv("ALGORITHM"))
-print(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
